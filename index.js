7c45ce34b07da5f8e7b2e814b8c711b91171668e
code-iddih <idinyamwaya@gmail.com>
2024-06-25 09:27:30 +0300

    Complete the tasks

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+// Create a variable called random that will generate a random integer greater than 0.
+
+const random = Math.ceil(Math.random() + 1);
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+let num3 = 40;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+let max = Math.max(1,2,3,4,5) + 15;
